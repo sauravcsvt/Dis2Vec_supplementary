d5102b99df277ab0570b605d343060f6de6097fc
sauravcsvt <sauravcsvt@vt.edu>
2017-05-08 16:46:08 -0400

    another commit

--- Dis2Vec_code/run_Dis2Vec.py
+++ Dis2Vec_code/run_Dis2Vec.py
@@ -18,22 +18,28 @@ import logging
 import argparse
 logging.basicConfig()
 
+class Dis2Vec(object):
 
-def perform_word2vec(sent_list, vec_dim, win_size, count_min, SG_negative, iter_corpus, spm, opm, smoothing, vocab_file):
+    def __init__(self, **kwargs):
+        self.params = kwargs
 
-    start_time = time.time()
-    model_HM = Word2Vec(sent_list, size=vec_dim, window=win_size, 
-                        min_count=count_min, sample=1e-05, workers=100,
-                        sg=1, hs=0, negative=SG_negative, iter=iter_corpus, sampling_param=spm, objective_param=opm, smoothing=smoothing, vocab_file=vocab_file)
-    end_time = time.time()
-    print "Total time taken is: " + str((end_time - start_time) / 3600.) + " hours"
-    out_folder = './output/'
-    if not os.path.isdir(out_folder):
-        os.makedirs(out_folder)
-    if model_HM.sample == 0:
-        model_HM.save(out_folder + 'model_all_vocab_' + str(vec_dim) + "_" + str(win_size) + "_" + str(count_min) + "_" + str(SG_negative) + "_" + str(spm) + "_" + str(opm) + "_" + str(smoothing) + "_" + str(iter_corpus) + '.word2vec')
-    else:
-        model_HM.save(out_folder + 'model_all_vocab_' + str(vec_dim) + "_" + str(win_size) + "_" + str(count_min) + "_" + str(SG_negative) + "_" + str(spm) + "_" + str(opm) + "_" + str(smoothing) + "_" + str(iter_corpus) + 'w_sample.word2vec')
+    def run_Dis2Vec(self):
+
+        start_time = time.time()
+        model_HM = Word2Vec(self.params["sent"], size=self.params["dim"], window=self.params["win"], 
+                            min_count=self.params["min_cnt"], sample=self.params["sample"], workers=100,
+                            sg=1, hs=0, negative=self.params["neg"], iter=self.params["iter"], 
+                            sampling_param=self.params["spm"], objective_param=self.params["opm"], 
+                            smoothing=self.params["smoothing"], vocab_file=self.params["vocab"])
+        end_time = time.time()
+        print "Total time taken is: " + str((end_time - start_time) / 3600.) + " hours"
+        out_folder = './output/'
+        if not os.path.isdir(out_folder):
+            os.makedirs(out_folder)
+        if model_HM.sample == 0:
+            model_HM.save(out_folder + 'model_Dis2Vec.word2vec')
+        else:
+            model_HM.save(out_folder + 'model_Dis2Vec_w_sample.word2vec')
 
 def parse_args():
 
@@ -47,14 +53,10 @@ def main():
     _arg = parse_args()
     sentences_corpus = pickle.load(open(_arg.inputcorpus, "r")) # Input corpus (list of sentences as input where each sentence is a list of tokens. file should be in .pkl format).
     domain_vocab_file = _arg.domainvocab # domain-specific vocabulary. file should be in .pkl format
-    param_list = {"dim": 300, "win": [15], "min_cnt": 5, "neg": [1, 5, 15], "iter": 1, "spm": [0.3, 0.5, 0.7], "opm": [0.3, 0.5, 0.7], "smoothing": [0.75, 1.0]}
-    for param_win in param_list['win']:
-        for param_neg in param_list['neg']:
-            for param_spm in param_list['spm']:
-                for param_opm in param_list['opm']:
-                    for param_smooth in param_list["smoothing"]:
-                        perform_word2vec(sentences_corpus, param_list["dim"], param_win, param_list["min_cnt"], 
-                                         param_neg, param_list["iter"], param_spm, param_opm, param_smooth, domain_vocab_file)
+    kwargs = {"sent": sentences_corpus, "vocab": domain_vocab_file, 
+              "dim": 300, "win": 15, "min_cnt": 5, "neg": 1, "iter": 1, 
+              "spm": 0.3, "opm": 0.7, "smoothing": 1.0, "sample": 1e-05}
+    Dis2Vec(**kwargs).run_Dis2Vec()
 
 if __name__ == "__main__":
     main()
