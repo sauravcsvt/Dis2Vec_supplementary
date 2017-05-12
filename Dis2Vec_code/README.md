2dfb0c888a7843b79156016b749938fb4e85f517
sauravcsvt <sauravcsvt@vt.edu>
2017-05-12 19:34:03 -0400

    some more addition

--- Dis2Vec_code/README.md
+++ Dis2Vec_code/README.md
@@ -1,4 +1,6 @@
-usage: run_Dis2Vec.py [-h] -ic INPUTCORPUS -v DOMAINVOCAB
+usage: run_Dis2Vec.py [-h] -ic INPUTCORPUS -v DOMAINVOCAB -d DIM -w WINDOW -n
+                      NEGATIVE -spm SAMPLINGPARAMETER -opm OBJECTIVEPARAMETER
+                      -sm SMOOTHING
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -9,5 +11,14 @@ optional arguments:
   -v DOMAINVOCAB, --domainvocab DOMAINVOCAB
                         Domain-specific vocabulary. file should be in .pkl
                         format
-
-                        
+  -d DIM, --dim DIM     Dimension of word embeddings (300, 600)
+  -w WINDOW, --window WINDOW
+                        Word window (5, 10, 15)
+  -n NEGATIVE, --negative NEGATIVE
+                        Number of negative samples (1, 5, 15)
+  -spm SAMPLINGPARAMETER, --samplingparameter SAMPLINGPARAMETER
+                        Sampling parameter (0.3, 0.5, 0.7)
+  -opm OBJECTIVEPARAMETER, --objectiveparameter OBJECTIVEPARAMETER
+                        Objective selection parameter (0.3, 0.5, 0.7)
+  -sm SMOOTHING, --smoothing SMOOTHING
+                        smoothing parameter (0.75, 1.0)
--- Dis2Vec_code/run_Dis2Vec.py
+++ Dis2Vec_code/run_Dis2Vec.py
@@ -16,6 +16,7 @@ from gensim.models import Word2Vec
 import time
 import logging
 import argparse
+import numpy as np
 logging.basicConfig()
 
 class Dis2Vec(object):
@@ -46,6 +47,12 @@ def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument("-ic", "--inputcorpus", type = str, required = True, help = "Input corpus which should be a list of sentences as input where each sentence is a list of tokens. file should be in .pkl format")
     ap.add_argument("-v", "--domainvocab", type = str, required = True, help = "Domain-specific vocabulary. file should be in .pkl format")
+    ap.add_argument("-d", "--dim", type = str, required = True, help = "Dimension of word embeddings (300, 600)")
+    ap.add_argument("-w", "--window", type = str, required = True, help = "Word window (5, 10, 15)")
+    ap.add_argument("-n", "--negative", type = str, required = True, help = "Number of negative samples (1, 5, 15)")
+    ap.add_argument("-spm", "--samplingparameter", type = str, required = True, help = "Sampling parameter (0.3, 0.5, 0.7)")
+    ap.add_argument("-opm", "--objectiveparameter", type = str, required = True, help = "Objective selection parameter (0.3, 0.5, 0.7)")
+    ap.add_argument("-sm", "--smoothing", type = str, required = True, help = "smoothing parameter (0.75, 1.0)")
     return ap.parse_args()
 
 def main():
@@ -53,9 +60,15 @@ def main():
     _arg = parse_args()
     sentences_corpus = pickle.load(open(_arg.inputcorpus, "r")) # Input corpus (list of sentences as input where each sentence is a list of tokens. file should be in .pkl format).
     domain_vocab_file = _arg.domainvocab # domain-specific vocabulary. file should be in .pkl format
+    dim = np.int(_arg.dim)
+    win = np.int(_arg.win)
+    neg = np.int(_arg.neg)
+    spm = np.float(_arg.spm)
+    opm = np.float(_arg.opm)
+    smoothing = np.float(_arg.smoothing)
     kwargs = {"sent": sentences_corpus, "vocab": domain_vocab_file, 
-              "dim": 300, "win": 15, "min_cnt": 5, "neg": 1, "iter": 1, 
-              "spm": 0.3, "opm": 0.7, "smoothing": 1.0, "sample": 1e-05}
+              "dim": dim, "win": win, "min_cnt": 5, "neg": neg, "iter": 1, 
+              "spm": spm, "opm": opm, "smoothing": smoothing, "sample": 1e-05}
     Dis2Vec(**kwargs).run_Dis2Vec()
 
 if __name__ == "__main__":
